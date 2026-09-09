35d57ed00ef70a19b4bc01d4301b7fb94356c0f7
Nagares, Wayne <t22-34844@student.mseuf.edu.ph>
2026-09-09 15:36:10 +0800

    Initial Commit

---
+++ midterm/midtermApp.swift
@@ -0,0 +1,17 @@
+//
+//  midtermApp.swift
+//  midterm
+//
+//  Created by Mac-LAB on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct midtermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
